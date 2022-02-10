f89fceaf9c297a809b6281d2cb632742f1ff97ef
michael.delva <michael@emidee.net>
2022-02-10 14:48:56 +0100

    Removed test suite

--- Source/MissionSystemTestSuite/Private/MissionSystemTestSuiteModule.cpp
+++
@@ -1,9 +0,0 @@
-#include "MissionSystemTestSuiteModule.h"
-
-#include "MissionSystemModule.h"
-
-class FMissionSystemTestSuiteModule final : public IMissionSystemModule
-{
-};
-
-IMPLEMENT_MODULE( FMissionSystemTestSuiteModule, MissionSystemTestSuite )
